42649d01d894bd7118fe03eb7327124677d68883
XavierF <xavier.farchetto@telecomnancy.eu>
2018-11-14 16:14:32 +0100

    Identification de toutes les étiquettes (tag)

--- pre_traitement.py
+++ pre_traitement.py
@@ -12,7 +12,7 @@ def verify_annotation(current_line):
 		line = "\t".join([word, "O"]) + "\n"
 	else:
 		annotation = words[1]
-		if annotation == "\n":
+		if annotation == "\n" or annotation == "":
 			annotation = "O"
 		if "\n" in annotation:
 			annotation = annotation[:-1]
@@ -24,21 +24,40 @@ def verify_annotation(current_line):
 			line = "\t".join([words[0], annotation]) + "\n"
 	return line
 
-def verify_file(input_directory, output_directory, file_name):
+def study_tag(line, list):
+	tag = line.split("\t")[1]
+	if not(tag in list):
+		list.append(tag)
+		return sorted(list)
+	else:
+		return list
+
+def verify_file(input_directory, output_directory, file_name, tags_list):
 	input_file_name = os.path.join(input_directory, file_name)
 	output_file_name = os.path.join(output_directory, file_name)
+	tag_list = tags_list
 	with open(output_file_name, "w") as output:
 		with open(input_file_name, "r") as input:
 			line = input.readline()
 			while line:
-				output.write(verify_annotation(line))
+				line = verify_annotation(line)
+				output.write(line)
+				tag_list = study_tag(line[:-1], tag_list)
 				line = input.readline()
+	return tag_list
+
+def write_tags_file(list):
+	with open("tags.txt", "w") as file :
+		for tag in list:
+			file.write(tag+"\n")
 
 def verify_directory(input_directory, output_directory, first_file=1, last_file=5428):
 	file_list = sorted(os.listdir(input_directory))[first_file-1:last_file]
+	tag_list = []
 	for counter, file in enumerate(file_list):
-		verify_file(input_directory, output_directory, file)
+		tag_list = verify_file(input_directory, output_directory, file, tag_list)
 		print("Element {}\{} - File {} reviewed".format(str(counter+first_file), last_file, file))
+	write_tags_file(tag_list)
 
 
 
